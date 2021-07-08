3a28edad172bb44a41f8b0a9fa0812c62907a74e
jaggbow <oussama.boussif@student.ecp.fr>
2021-07-08 07:07:08 -0400

    Formatted the file according to pep8

--- scripts/dedalus-gif-generator.py
+++ scripts/dedalus-gif-generator.py
@@ -1,15 +1,15 @@
 import argparse
-import numpy as np
 import os
-import h5py
-import matplotlib.pyplot as plt
 
+import h5py
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description="Data generation script for 2D Rayleigh-Benard convection using Dedalus"
+        description="Script that generates animations from h5 dataset generated using dedalus"
     )
     parser.add_argument(
         "--file",
@@ -18,13 +18,13 @@ def get_args():
     )
     parser.add_argument(
         "--variable",
-        default='T',
+        default="T",
         type=str,
         help="Specify which variable you want displayed, one of [T,p,u,w]. (default: 'T')",
     )
     parser.add_argument(
         "--dst",
-        default='.',
+        default=".",
         type=str,
         help="Path to Destination directory. (default: '.')",
     )
@@ -36,7 +36,7 @@ def get_args():
     )
     parser.add_argument(
         "--cmap",
-        default='RdBu_r',
+        default="RdBu_r",
         type=str,
         help="Colormap of the displayed quantities. (default: 'RdBu_r')",
     )
@@ -52,12 +52,17 @@ if __name__ == "__main__":
     dst = args.dst
     phy_var = args.variable
 
-    assert phy_var in ['T', 'u', 'p', 'w'], "Physical variable should be one of ['T', 'p', 'u', 'w']"
-    data = h5py.File(path, 'r')
+    assert phy_var in [
+        "T",
+        "u",
+        "p",
+        "w",
+    ], "Physical variable should be one of ['T', 'p', 'u', 'w']"
+    data = h5py.File(path, "r")
     # First set up the figure, the axis, and the plot element we want to animate
     fig = plt.figure(figsize=(8, 4))
 
-    variable = np.transpose(data['tasks'][phy_var][:], (0, 2, 1))
+    variable = np.transpose(data["tasks"][phy_var][:], (0, 2, 1))
     n_frames = variable.shape[0]
     im = plt.imshow(variable[0], cmap=cmap)
     plt.colorbar(im)
@@ -65,10 +70,12 @@ if __name__ == "__main__":
     def animate_func(i):
         im.set_array(variable[i])
         return [im]
+
     anim = animation.FuncAnimation(
-                                fig, 
-                                animate_func, 
-                                frames = n_frames,
-                                interval = 1000 / fps, # in ms
-                                )
-    anim.save(os.path.join(args.dst, f'animation_{phy_var}.gif'), fps=fps)
+        fig,
+        animate_func,
+        frames=n_frames,
+        interval=1000 / fps,  # in ms
+    )
+    plt.show()
+    anim.save(os.path.join(args.dst, f"animation_{phy_var}.gif"), fps=fps)
