321b008cefffdf65bc6bd1a7b3d991a51d1a3c65
vict0rsch <vsch@pm.me>
2021-07-12 10:34:21 +0200

    add train_val_split

--- scripts/make_density_json.py
+++ scripts/make_density_json.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 from minydra import Parser
+import numpy as np
 from tqdm import tqdm
 
 from .subsample_density_dataset import label_file
@@ -14,6 +15,7 @@ if __name__ == "__main__":
 
     base = Path("/network/tmp1/schmidtv/perovai")
     out = Path("/network/tmp1/schmidtv/perovai/labeled_data_paths.json")
+    train_split = 0.8
 
     if "base" in args:
         base = Path(args.base).expanduser().resolve()
@@ -23,6 +25,8 @@ if __name__ == "__main__":
             print(str(out), "already exists")
             if "y" not in input("Continue? [y/n]"):
                 sys.exit()
+    if "train_split" in args:
+        train_split = float(args.train_split)
 
     assert out.parent.exists()
 
@@ -33,7 +37,20 @@ if __name__ == "__main__":
 
     data_files = [f for f in tqdm(base.glob(f"**/{fname}"))]
 
-    jfiles = {f: label_file(f) for f in tqdm(data_files)}
+    perm = np.random.permutation(len(data_files))
+    train_idx = int(train_split * len(data_files))
 
-    with out.open("w") as f:
-        json.dump(jfiles, out)
+    train_jfiles = {
+        data_files[k]: label_file(data_files[k]) for k in tqdm(perm[:train_idx])
+    }
+    val_jfiles = {
+        data_files[k]: label_file(data_files[k]) for k in tqdm(perm[train_idx:])
+    }
+
+    train_out = out.parent / f"train_{out.name}"
+    val_out = out.parent / f"val_{out.name}"
+
+    with train_out.open("w") as f:
+        json.dump(train_jfiles, f)
+    with val_out.open("w") as f:
+        json.dump(val_jfiles, f)
